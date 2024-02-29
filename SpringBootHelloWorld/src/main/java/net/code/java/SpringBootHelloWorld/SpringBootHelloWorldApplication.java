829236f7369136ce4cfcf3fb7b212439a08365d0
N1CKZ3B <143991834+N1CKZ3B@users.noreply.github.com>
2024-02-29 17:37:13 -0500

    Add files via upload

---
+++ SpringBootHelloWorld/src/main/java/net/code/java/SpringBootHelloWorld/SpringBootHelloWorldApplication.java
@@ -0,0 +1,26 @@
+package net.code.java.SpringBootHelloWorld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringBootHelloWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootHelloWorldApplication.class, args);
+	}
+
+	@RequestMapping("/")
+    public String home() {
+        return "<h1>Hello world Spring Boot</h1><p>Welcome to my Spring Boot application!</p>";
+    }
+
+    @RequestMapping("/new")
+    public String newMessage() {
+        return "<h2>This is a new message</h2><p>Welcome to the new message page!</p>";
+    }
+
+}
---
+++ SpringBootHelloWorld/src/test/java/net/code/java/SpringBootHelloWorld/SpringBootHelloWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package net.code.java.SpringBootHelloWorld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootHelloWorldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
